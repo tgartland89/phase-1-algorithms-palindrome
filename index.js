02fd95d14da8b94abfb0d045ef1f9db0fa3fcdac
tgartland89 <thomas.gartland@gmail.com>
2023-05-05 18:37:04 -0600

    index.JS for alog palindrome lab

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word) {
+ return word.split("").reverse().join("");
 }
 
-/* 
-  Add your pseudocode here
-*/
+function isPalindrome(word) {
+  const reversedWord = reverse(word);
 
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
+  return word === reversedWord;
+}
 if (require.main === module) {
-  // add your own custom tests in here
+  
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -20,6 +16,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
 }
 
 module.exports = isPalindrome;
